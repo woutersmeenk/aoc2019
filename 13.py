ab2ef5d4e238440ad71b884c1cbac94c10753e27
Wouter Smeenk <woutersmeenk@gmail.com>
2019-12-13 08:51:01 +0100

    Day 13 now with AI!

--- 13.py
+++ 13.py
@@ -1,6 +1,5 @@
 from intcode_comp import IntcodeComp
 from collections import namedtuple, defaultdict
-import keyboard
 import time
 import os
 
@@ -10,28 +9,36 @@ def print_screen(screen, raw_out):
         p = (raw_out[point_idx], raw_out[point_idx + 1])
         screen[p] = raw_out[point_idx + 2]
 
+    ball_x = 0
+    paddle_x = 0
     print(f"Score: {screen[(-1,0)]}")
     for y in range(0, 26):
         for x in range(0, 42):
             tile = screen[(x, y)]
             if tile == 0:
-                print(" ", end="")
+                #print(" ", end="")
+                pass
             elif tile == 1:
-                print("#", end="")  # wall
+                # print("#", end="")  # wall
+                pass
             elif tile == 2:
-                print("@", end="")  # block
+                # print("@", end="")  # block
+                pass
             elif tile == 3:
-                print("_", end="")  # paddle
+                # print("_", end="")  # paddle
+                paddle_x = x
             elif tile == 4:
-                print("o", end="")  # ball
-        print(" ")
+                # print("o", end="")  # ball
+                ball_x = x
+        #print(" ")
+    return (ball_x, paddle_x)
 
 
 if __name__ == "__main__":
     lines = open("13.dat", "r").readlines()
     prog = list(map(int, lines[0].split(',')))
 
-    seconds_per_frame = 0.5
+    seconds_per_frame = 0
     frame_start = time.perf_counter()
     screen = defaultdict(lambda: 0)
     comp = IntcodeComp(prog)
@@ -39,26 +46,21 @@ if __name__ == "__main__":
 
     raw_out = comp.run_until_input([])
 
-    joystick = 0
     while not comp.halted:
         #os.system('cls' if os.name == 'nt' else 'clear')
-        print_screen(screen, raw_out)
+        ball_x, paddle_x = print_screen(screen, raw_out)
+        if ball_x < paddle_x:
+            joystick = -1
+        elif ball_x > paddle_x:
+            joystick = 1
+        else:
+            joystick = 0
         frame_end = time.perf_counter()
         frame_time = frame_end - frame_start
-        print(f"Frame render time (s): {frame_time}")
+        #print(f"Frame render time (s): {frame_time}")
         sleep_time = max(0, seconds_per_frame - frame_time)
         time.sleep(sleep_time)
         frame_start = time.perf_counter()
         raw_out = comp.run_until_input([joystick])
-        print(f"Joystick: ", end="")
-        while True:
-            if keyboard.is_pressed('left arrow'):
-                joystick = -1
-                break
-            elif keyboard.is_pressed('right arrow'):
-                joystick = 1
-                break
-            elif keyboard.is_pressed('down arrow'):
-                joystick = 0
-                break
-        print(f"{joystick}")
+
+    print_screen(screen, raw_out)
