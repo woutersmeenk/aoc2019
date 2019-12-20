695857b8b5ba524cf4feab756633a37977eb5660
Wouter Smeenk <woutersmeenk@gmail.com>
2019-12-20 14:59:45 +0100

    Day 15 Part 1 and 2

--- 15.py
+++ 15.py
@@ -1,6 +1,7 @@
 from intcode_comp import IntcodeComp
 from graph_tools import *
 from collections import defaultdict
+import curses
 
 
 def neighbores(loc):
@@ -12,7 +13,22 @@ def neighbores(loc):
     return result
 
 
-if __name__ == "__main__":
+def draw_screen(stdscr, loc_status, loc, dest_loc):
+    term_max_y, term_max_x = curses.LINES - 1, curses.COLS - 2
+    for y in range(0, term_max_y):
+        for x in range(0, term_max_x):
+            print_loc = (loc[0]+x-term_max_x//2, loc[1]+y-term_max_y//2)
+            if loc == print_loc:
+                stdscr.addch(y, x, 'D')
+            elif dest_loc == print_loc:
+                stdscr.addch(y, x, '@')
+            else:
+                stdscr.addch(y, x, loc_status[print_loc])
+    stdscr.addch(term_max_y, term_max_x, ".")
+    stdscr.refresh()
+
+
+def main(stdscr):
     lines = open("15.dat", "r").readlines()
     prog = list(map(int, lines[0].split(',')))
 
@@ -26,6 +42,8 @@ if __name__ == "__main__":
     loc_status = defaultdict(lambda: ' ')
     loc_status[loc] = '.'
 
+    stdscr.clear()
+    sys_loc = None
     while not comp.halted:
         new_loc = neighbores(loc)[direction - 1]
         droid_status = comp.run_until_input([direction])[0]
@@ -41,12 +59,20 @@ if __name__ == "__main__":
                     map_graph[loc].add(neig)
                     map_graph[neig].add(loc)
         elif droid_status == 2:
-            break
+            loc_status[new_loc] = 'O'
+            loc = new_loc
+            sys_loc = loc
+            for neig in neighbores(loc):
+                if loc_status[neig] != '#':
+                    map_graph[loc].add(neig)
+                    map_graph[neig].add(loc)
         dists = dijkstra(map_graph, loc)
         unvisted_dists = {l: dists[l]
                           for l in dists.keys() if loc_status[l] == ' '}
-        min_unvisted = min(unvisted_dists, key=unvisted_dists.get)
-        next_on_path = min_unvisted
+        if len(unvisted_dists) == 0:
+            break
+        dest_loc = min(unvisted_dists, key=unvisted_dists.get)
+        next_on_path = dest_loc
         while dists[next_on_path] > 1:
             neig_dists = {neig: dists[neig]
                           for neig in neighbores(next_on_path) if neig in dists}
@@ -56,5 +82,27 @@ if __name__ == "__main__":
             if loc_neigs[d] == next_on_path:
                 direction = d + 1
                 break
+        draw_screen(stdscr, loc_status, loc, dest_loc)
 
-    print(f"Part 1 {dists[(0,0)]}")
+    dists = dijkstra(map_graph, sys_loc)
+    stdscr.addstr(0, 0, f"Part 1: {dists[(0,0)]}")
+    stdscr.getkey()
+
+    ox_locs = [sys_loc]
+    time = 0
+    while len(ox_locs) > 0:
+        time += 1
+        new_ox_locs = []
+        for ox_loc in ox_locs:
+            for neig in neighbores(ox_loc):
+                if loc_status[neig] == '.':
+                    new_ox_locs.append(neig)
+                    loc_status[neig] = 'O'
+        ox_locs = new_ox_locs
+        draw_screen(stdscr, loc_status, loc, None)
+        stdscr.addstr(0, 0, f"Part 2: {time}")
+    stdscr.getkey()
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
